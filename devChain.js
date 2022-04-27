0892d6528df1355cba951c4006a53f615a9bd577
Dylon Wong <b8065261@newcastle.ac.uk>
2022-04-27 15:00:00 +0100

    Use random mnemonic for test chain

--- devChain.js
+++ devChain.js
@@ -5,7 +5,7 @@ const run = () => {
   console.log("Starting local dev chain...");
   const isTest = process.argv.slice(2).includes("--test");
   const port = isTest ? 8545 : 7545;
-  const mnemonic = isTest ? ["-m", `"${process.env.GANACHE_MNEMONIC}"`] : [];
+  const mnemonic = isTest ? [] : ["-m", `"${process.env.GANACHE_MNEMONIC}"`];
   try {
     spawn("ganache-cli", ["-i", "1337", "--port", port].concat(mnemonic), {
       shell: true,
